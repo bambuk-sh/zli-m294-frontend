6518d70038595b21cc7d071fa630e3ccf43bda0a
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-02-02 10:45:26 +0100

    checkbox calls function

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from "react";
 
 function Tasks() {
     const [taskarr, setTaskarr] = useState([]);
-    const [checked, setChecked] = useState([]);
+    const [checked, setChecked] = useState(false);
     function getTasks() {
         http.get('/tasks').then(response => {
             setTaskarr(response.data);
@@ -16,28 +16,39 @@ function Tasks() {
         getTasks();
     }, []);
 
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setChecked(event.target.checked);
+        console.log('check: ' + event.target.checked + ' id: ' + event.target.getAttribute('id'));
+    }
+
     return (
         <div className='Tasks'>
             <table>
-                <tr>
-                    <th>Task ID</th>
-                    <th>Task</th>
-                    <th>Completed</th>
-                </tr>
-                {taskarr.map((item: Task) => (
-                    <tr>
-                        <td>{item.id}</td>
-                        <td>{item.title}</td>
-                        <td>
-                            {
-                                React.createElement('input',{
-                                    type: 'checkbox',
-                                    defaultChecked: item.completed
-                                })
-                            }
-                        </td>
+                <thead>
+                    <tr id='task_head'>
+                        <th>Task ID</th>
+                        <th>Task</th>
+                        <th>Completed</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {taskarr.map((item: Task) => (
+                        <tr id={'task_id_' + item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>
+                                {
+                                    React.createElement('input', {
+                                        type: 'checkbox',
+                                        defaultChecked: item.completed,
+                                        id: item.id,
+                                        onChange: handleChange
+                                    })
+                                }
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div >
     );

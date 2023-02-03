6c33d09a280b1fbc458557ef71064d2aa1962c2d
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-02-03 08:36:44 +0100

    Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './pico.classless.css'
 import Tasks from './components/Tasks';
 import TaskAdd from './components/task_add';
+import Task_table from './components/task_table';
 
 function App() {
   return (
     <div className="App">
-      <Tasks />
+      <Task_table />
       <TaskAdd />
     </div>
   );
--- src/components/task_table.tsx
+++ src/components/task_table.tsx
@@ -1,11 +1,42 @@
 import Task from "../types/task_type";
 import http from "../http";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Task_table() {
+    const [taskarr, setTaskarr] = useState<[] | Task[]>([]);
+
+    useEffect(() => {
+        http.get<Task[]>('/tasks').then((result) => {
+            setTaskarr(result.data);
+        });
+    }, []);
+
     return (
         <div className="task_table">
-
+            <table>
+                <thead>
+                    <tr>
+                        <th>Task ID</th>
+                        <th>Title</th>
+                        <th>Completed</th>
+                        <th colSpan={3}>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {taskarr.map((item: Task) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>
+                                <input type='checkbox' defaultChecked={item.completed} />
+                            </td>
+                            <td><button>View</button></td>
+                            <td><button>Edit</button></td>
+                            <td><button>Delete</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
